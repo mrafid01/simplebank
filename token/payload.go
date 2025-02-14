59394658ad0ea66e1399303063e607476f576fe0
mrafid01 <rafid.muhammad12@gmail.com>
2025-02-14 22:14:34 +0700

    add create and verify token using JWT and Paseto

---
+++ token/payload.go
@@ -0,0 +1,45 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+type Payload struct {
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now()
+	payload := &Payload{
+		ID:       tokenID,
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(now),
+			ExpiresAt: jwt.NewNumericDate(now.Add(duration)),
+		},
+	}
+	return payload, nil
+}
+
+func (p *Payload) Valid() error {
+	if p.ExpiresAt == nil || time.Now().After(p.ExpiresAt.Time) {
+		return ErrExpiredToken
+	}
+	return nil
+}
